6733fd6a2724dac0200a0c5c1cfda3d5dedc091f
Ahead1Face <j5p7l@mail.ru>
2022-05-13 10:22:58 +0700

    add image in store window

---
+++ GameStore/CustomControll/StoreBorder.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameStore.CustomControll
+{
+    /// <summary>
+    /// Логика взаимодействия для StoreBorder.xaml
+    /// </summary>
+    public partial class StoreBorder : UserControl
+    {
+        Window parent;
+        public StoreBorder(Window parent)
+        {
+            this.parent = parent;
+            InitializeComponent();
+        }
+        private void bHide_Click(object sender, RoutedEventArgs e)
+        {
+            this.parent.WindowState = WindowState.Minimized;
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.parent.Close();
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                parent.DragMove();
+        }
+
+        private void MinimaizeBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (parent.WindowState == WindowState.Maximized)
+                parent.WindowState = WindowState.Normal;
+            else
+                parent.WindowState = WindowState.Maximized;
+        }
+    }
+}
--- GameStore/Vews/StoreWindow.xaml.cs
+++ GameStore/Vews/StoreWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using GameStore.CustomControll;
+using GameStore.Model;
 
 namespace GameStore.Vews
 {
@@ -22,6 +24,66 @@ namespace GameStore.Vews
         public StoreWindow()
         {
             InitializeComponent();
+            StoreBorder windowBorder = new StoreBorder(this);
+            windowBorder.SetValue(Grid.RowProperty, 0);
+            StoreGrid.Children.Add(windowBorder);
+            FillGrid();
+        }
+        void FillGrid()
+        {
+            using(DBStore db = new DBStore())
+            {
+                List<RowDefinition> rows = new List<RowDefinition>();
+                int gameCount = db.Game.Count();
+                int rowsCount = (gameCount +1) / 2;
+                for(int i = 0; i < rowsCount; i++)
+                {
+                    rows.Add(new RowDefinition());
+                    rows[i * 2].Height = new GridLength(30);
+                    rows.Add(new RowDefinition());
+                    rows[i * 2 + 1].Height = new GridLength(130, GridUnitType.Star);
+                }
+                rows.Add(new RowDefinition());
+                rows[rowsCount - 1].Height = new GridLength(30);
+
+                for (int i = 0; i < rows.Count; i++)
+                {
+                    GameGrid.RowDefinitions.Add(rows[i]);
+                }
+
+                int columnNum = 1;
+                int rowNum = 1;
+                int index = 0;
+                foreach(Game game in db.Game)
+                {
+                    StackPanel sp = new StackPanel();
+                    sp.SetValue(Grid.RowProperty, rowNum);
+                    sp.SetValue(Grid.ColumnProperty, columnNum);
+
+                    Label price = new Label();
+                    price.HorizontalAlignment = HorizontalAlignment.Center;
+                    price.Content = game.Price + "$";
+
+                    Label Name = new Label();
+                    Name.HorizontalAlignment = HorizontalAlignment.Center;
+                    Name.Content = game.Name;
+                    Image currentImage = new Image();
+                    BitmapImage Logo = DataTransform.ByteToImage(game.Image);
+                    currentImage.Source = Logo;
+                    columnNum = (columnNum == 1) ? 3 : 1;
+
+                    sp.Children.Add(currentImage);
+                    sp.Children.Add(Name);
+                    sp.Children.Add(price);
+                    GameGrid.Children.Add(sp);
+
+                    if((index +1) %2 == 0)
+                    {
+                        rowNum += 2;
+                    }
+                    index++;
+                }
+            }
         }
     }
 }
